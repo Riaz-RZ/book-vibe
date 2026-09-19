b95ebbcf6086cf0b043a10be1b774ba8134b9e69
Riaz-RZ <riazacc89@gmail.com>
2026-09-20 00:55:47 +0600

    Home page Banner section Created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import Banner from "@/components/homepage/Banner";
 
 const page = () => {
   return (
-    <div>
-      <h1>Home page</h1>
+    <div className="container mx-auto">
+      <Banner/>
     </div>
   );
 };
--- src/components/homepage/Banner.tsx
+++ src/components/homepage/Banner.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import Image from "next/image";
+import bannerimage from "@/assets/banner-logo.png"
 
 const Banner = () => {
     return (
-        <div>
-            <h1>Banner for homepage</h1>
+        <div className="bg-gray-100 grid grid-cols-3 py-10 rounded-2xl mt-4">
+            <div className="col-span-2">
+                <h2 className="text-7xl ps-20 pt-8">Books to freshen up <br /> your bookself</h2>
+                <button className="btn bg-green-500 text-white ms-20 mt-10 text-xl">View The List</button>
+            </div>
+            <div className="col-span-1">
+                <Image src={bannerimage} alt="Banner Image" />
+            </div>
         </div>
     );
 };
